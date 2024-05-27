654b93585e99c468e9f5dbfae43e6877e16c59e9
Андрей Сорокин <andrewsorokin540@gmail.com>
2024-05-27 12:21:32 +0700

    implement chess board drawing

--- hw03_chessboard/main.go
+++ hw03_chessboard/main.go
@@ -1,5 +1,19 @@
 package main
 
+import (
+	"fmt"
+	"github.com/AndrewSorokin540/home_work_basic/hw03_chessboard/drawer"
+)
+
 func main() {
-	// Place your code here.
+	var size int
+	fmt.Printf("Enter chess board size: ")
+	_, scanErr := fmt.Scanf("%d", &size)
+
+	if scanErr != nil {
+		fmt.Printf("Error: %v", scanErr)
+		return
+	}
+
+	chessBoardDrawer.DrawChessBoard(size)
 }
